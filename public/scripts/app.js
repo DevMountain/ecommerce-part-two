e0e8a79c3e79f4e680c743163194fe5396b62582
Jason Turner <jasonalmaturner@gmail.com>
2015-06-26 14:20:38 -0600

    working on front end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,35 @@
-var app = angular.module('ecommerce', []);
+var app = angular.module('ecommerce', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/register', {
+    templateUrl: 'views/register/registerTmpl.html',
+    controller: 'registerCtrl',
+    resolve: {
+      users: function(adminService){
+          return adminService.getUsers();
+        }
+    }
+  })
+  .when('/shopping', {
+    templateUrl: 'views/shopping/shoppingTmpl.html',
+    controller: 'shoppingCtrl',
+    resolve: {
+      products: function(mainService){
+        return mainService.getProducts();
+      }
+    }
+  })
+  .when('/admin', {
+    templateUrl: 'views/shopping/shoppingTmpl.html',
+    controller: 'adminCtrl',
+    resolve: {
+      products:function(mainService){
+        return mainService.getProducts()
+      }
+    }
+  })
+  .otherwise({
+    redirectTo: '/shopping'
+  });
+});
